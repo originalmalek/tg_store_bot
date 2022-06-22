63d5ea334a4343ef2cfab068a43875e230c06df4
originalmalek <56593369+originalmalek@users.noreply.github.com>
2022-06-22 13:32:56 +0200

    Add files via upload

---
+++ telegram_logger.py
@@ -0,0 +1,13 @@
+import telebot
+import logging
+
+
+class MyLogsHandler(logging.Handler):
+    def __init__(self, level=logging.NOTSET, telegram_token=None, chat_id=None):
+        self.bot = telebot.TeleBot(telegram_token)
+        self.chat_id = chat_id
+        super().__init__(level=level)
+
+    def emit(self, record):
+        self.log_entry = self.format(record)
+        self.bot.send_message(chat_id=self.chat_id, text=self.log_entry)
